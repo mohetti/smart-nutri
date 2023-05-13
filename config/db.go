e6f7efbdcfee2b924630578ce9265906593dd093
Moritz Hettich <moritz.hettich@ebalance.ch>
2023-05-13 19:16:32 +0200

    Use env

--- config/db.go
+++ config/db.go
@@ -3,15 +3,15 @@ package config
 import (
     "fmt"
     "os"
-    "context"
-	"github.com/jackc/pgx/v5/pgxpool"
+    _ "github.com/lib/pq"
+	"github.com/jmoiron/sqlx"
 )
 
-var DB *pgxpool.Pool
+var DB *sqlx.DB
 
 func DBInit() {
     var err error
-	DB, err = pgxpool.New(context.Background(), os.Getenv("DATABASE_URL"))
+	DB, err = sqlx.Connect("postgres", os.Getenv("POSTGRES_CREDENTIALS"))
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
 		os.Exit(1)
